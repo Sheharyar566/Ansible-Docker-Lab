cf3194173d31fe6632d722c61b090e1b63c6dbc8
apple <apple@MacBook-Pro.local>
2025-04-13 23:23:39 +0500

    Updated the docker-compose to remove version, added a bash script to ease the setup

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d "venv" ]; then
+    echo "Creating python environment"
+    python3 -m venv venv
+fi
+
+if [ ! -f "ansible" ] || [ ! -f "ansible.pub" ]; then
+    echo "Creating ansible keys"
+    ssh-keygen -C "ansible" -f "$PWD/ansible"
+fi
+
+source venv/bin/activate
+
+if ! pip show ansible > /dev/null 2>&1; then
+    echo "Installing ansible"
+    pip install ansible
+fi
+
+docker compose up --build -d
+
+for i in $(seq 1 3)
+do
+    PORT="222$i"
+    echo $PORT
+    ssh-copy-id -i "$PWD/ansible.pub" -p $PORT root@localhost
+done
